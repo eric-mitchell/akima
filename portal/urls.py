8e6e9875141f1d592fae83c9ffdd0efa5c607337
Rob Z <rzwink@gmail.com>
2020-09-05 11:52:48 -0400

    add save

--- portal/urls.py
+++ portal/urls.py
@@ -1,9 +1,11 @@
 # urls.py
+from django.shortcuts import redirect
 from django.urls import path
 
 from portal.views import ArticleList, ArticleDetail, SaveArticleAnnotations
 
 urlpatterns = [
+    path("", lambda req: redirect("/articles/")),
     path("articles/", ArticleList.as_view(), name="article-list"),
     path("articles/<str:pk>/", ArticleDetail.as_view(), name="author-detail"),
     path(
